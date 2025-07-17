537e9ed49b9920c076358fedfb8df5ccad0da034
yashh8649 <yashgundewadi5@gmail.com>
2025-07-17 10:13:07 +0530

    New changes

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let [seconds, minutes, hours] = [0, 0, 0];
+let [milliseconds, seconds, minutes] = [0, 0, 0];
 let display = document.getElementById("display");
 let interval = null;
 
 document.getElementById("start").onclick = () => {
   if (interval) return;
-  interval = setInterval(startTimer, 1000);
+  interval = setInterval(startTimer, 10); // update every 10ms
 };
 
 document.getElementById("pause").onclick = () => {
@@ -15,7 +15,7 @@ document.getElementById("pause").onclick = () => {
 document.getElementById("reset").onclick = () => {
   clearInterval(interval);
   interval = null;
-  [seconds, minutes, hours] = [0, 0, 0];
+  [milliseconds, seconds, minutes] = [0, 0, 0];
   display.innerText = "00:00:00";
   document.getElementById("laps").innerHTML = "";
 };
@@ -28,19 +28,18 @@ document.getElementById("lap").onclick = () => {
 };
 
 function startTimer() {
-  seconds++;
+  milliseconds += 10;
+  if (milliseconds === 1000) {
+    milliseconds = 0;
+    seconds++;
+  }
   if (seconds === 60) {
     seconds = 0;
     minutes++;
   }
-  if (minutes === 60) {
-    minutes = 0;
-    hours++;
-  }
-
-  let h = hours < 10 ? "0" + hours : hours;
   let m = minutes < 10 ? "0" + minutes : minutes;
   let s = seconds < 10 ? "0" + seconds : seconds;
-
-  display.innerText = `${h}:${m}:${s}`;
+  let ms = Math.floor(milliseconds / 10);
+  ms = ms < 10 ? "0" + ms : ms;
+  display.innerText = `${m}:${s}:${ms}`;
 }
